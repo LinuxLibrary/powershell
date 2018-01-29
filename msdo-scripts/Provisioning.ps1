1312c28f69301619511b3c64c4b6298083fd3cae
Arjun Shrinivas <vmsnivas@gmail.com>
2018-01-29 18:42:48 +0530

    Modified Provisioning.ps1

--- msdo-scripts/Provisioning.ps1
+++ msdo-scripts/Provisioning.ps1
@@ -2,4 +2,10 @@
 
 Find-Module | Select-String -Pattern "<SomeName>" -SimpleMatch
 
-# 
+# Create virtual machine from template
+
+New-VM -Name <Some_Name> -Template <Template_Name> -VMHost <VM_Host_Name>
+
+# Start virtual machine
+
+Start-VM -Name <VM_Name>
