8c5752bf8f20d6af60d9fbc24415d43cbc2b5d56
Arjun Shrinivas <vmsnivas@gmail.com>
2018-03-05 12:02:53 +0530

    Modified MSDevOps_Scripts/SpinVM.ps1

--- scripts/MSDevOps_Scripts/SpinVM.ps1
+++ scripts/MSDevOps_Scripts/SpinVM.ps1
@@ -28,22 +28,44 @@ $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
 $source = '\\150.150.150.78\ftp'
 $destination = 'c:\FTPFiles'
-$script1 = "robocopy $source $destination /MIR /E /FFT"
-$script2 = "Start-Process msiexec.exe -ArgumentList @('/qn', '/lv C:\FTPFiles\chef-log.txt', '/i C:\FTPFiles\chefclient.msi') -NoNewWindow -Wait"
-$script3 = "New-Item -ItemType Directory -Path C:\chef\.chef
-Copy-Item C:\FTPFiles\a-cf-validator.pem C:\chef\.chef\
+$scriptcopy = "robocopy $source $destination /MIR /E /FFT"
+$scriptchef = "Start-Process msiexec.exe -ArgumentList @('/qn', '/lv C:\FTPFiles\chef-log.txt', '/i C:\FTPFiles\chefclient.msi') -NoNewWindow -Wait"
+$scriptmkd = "New-Item -ItemType Directory -Path C:\chef
+New-Item -ItemType Directory -Path C:\chef\.chef
+New-Item -ItemType Directory -Path  c:\OctopusDSC"
+$scripttenta = "Start-Process msiexec.exe -ArgumentList @('/qn', '/lv C:\FTPFiles\tentacle-log.txt', '/i C:\FTPFiles\Tentacle.msi') -NoNewWindow -Wait"
+$scriptnug = "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force"
+$scriptOcto = "Save-Module -Name OctopusDSC -Path c:\OctopusDSC -RequiredVersion 3.0.169
+Install-Module -Name OctopusDSC -RequiredVersion 3.0.169 -force "
+$scriptmove = "Copy-Item C:\FTPFiles\a-cf-validator.pem C:\chef\.chef\
 Copy-Item C:\FTPFiles\sri321.pem C:\chef\
 Copy-Item C:\FTPFiles\knife.rb C:\chef\
-Copy-Item C:\FTPFiles\client.rb C:\chef\"
+Copy-Item C:\FTPFiles\client.rb C:\chef
+Copy-Item C:\FTPFiles\OctopusTools C:\ -recurse"
+$octoconf = "c:\FTPFiles\octoconf.ps1"
+
 $old_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$new_path = $old_path + ';' + 'C:\opscode\chef\bin\'
-$script4 = "Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value '$new_path'"
-Invoke-VMScript -ScriptText $script1 -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+$new_path = $old_path + ';' + 'C:\opscode\chef\bin\'+';'+'C:\Program Files\Octopus Deploy\Tentacle'
+
+$scriptset = "Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH â€“Value '$new_path'"
+
+Invoke-VMScript -ScriptText $scriptcopy -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
 Start-Sleep 60
-Invoke-VMScript -ScriptText $script2 -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Invoke-VMScript -ScriptText $scriptmkd -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 10
+Invoke-VMScript -ScriptText $scriptmove -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 20
+Invoke-VMScript -ScriptText $scriptchef -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
 Start-Sleep 300
-Invoke-VMScript -ScriptText $script3 -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
-Start-Sleep 10
-Invoke-VMScript -ScriptText $script4 -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
-Start-Sleep 10
-Invoke-Command -ComputerName $myIP -ScriptBlock {C:\opscode\chef\bin\chef-client.bat} -Credential $cred
+Invoke-VMScript -ScriptText $scripttenta -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 40
+Invoke-VMScript -ScriptText $scriptnug -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 30
+Invoke-VMScript -ScriptText $scriptOcto -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 40
+Invoke-VMScript -ScriptText $octoconf -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 30
+Invoke-VMScript -ScriptText $scriptset -VM $DevDep1 -GuestUser $UserName -GuestPassword $Password
+Start-Sleep 20
+Invoke-Command -ComputerName $myIP -ScriptBlock {C:\opscode\chef\bin\chef-client.bat} -Credential $Cred
+
