cea0683baad656f20ec250de8e7b33e604b7b7e3
Arjun Shrinivas <vmsnivas@gmail.com>
2018-03-05 17:52:54 +0530

    Added MSDevOps_Scripts/DepTarget.ps1

---
+++ scripts/MSDevOps_Scripts/DepTarget.ps1
@@ -0,0 +1,21 @@
+﻿Add-Type -Path 'Newtonsoft.Json.dll'
+Add-Type -Path 'Octopus.Client.dll' 
+ 
+$octopusApiKey = $OctopusAPI
+$octopusURI = $OctopusURL
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $octopusURI, $octopusApiKey
+$repository = new-object Octopus.Client.OctopusRepository $endpoint
+
+$tentacle = New-Object Octopus.Client.Model.MachineResource
+
+$tentacle.name = Hostname
+$tentacle.EnvironmentIds.Add("$OctoEnv")
+$tentacle.Roles.Add("$OctoRole")
+
+$tentacleEndpoint = New-Object Octopus.Client.Model.Endpoints.ListeningTentacleEndpointResource
+$tentacle.EndPoint = $tentacleEndpoint
+$tentacle.Endpoint.Uri = "https://$TentacleIP:10933"
+$tentacle.Endpoint.Thumbprint = "$TentacleThumbPrint"
+
+$repository.machines.create($tentacle)
