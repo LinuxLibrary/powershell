fd35464e330cd5a90f0202acf4a34028f9238f2a
Arjun Shrinivas <vmsnivas@gmail.com>
2018-03-05 17:52:54 +0530

    Added MSDevOps_Scripts/Get_Tentacle_ThumbPrint.ps1

---
+++ scripts/MSDevOps_Scripts/Get_Tentacle_ThumbPrint.ps1
@@ -0,0 +1,4 @@
+﻿$file = "C:\Octopus\Tentacle.config"
+$xml = [xml](Get-Content $file)
+$Tentacle_ThumbPrint = ($xml.'octopus-settings'.set | Where-Object {$_.key -eq 'Tentacle.CertificateThumbprint'}).'#text'
+$Tentacle_ThumbPrint
