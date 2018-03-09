49ccf20212ebdc6e6b020d30ea7652ec5a3fa90e
Arjun Shrinivas <vmsnivas@gmail.com>
2018-03-09 12:21:52 +0530

    Added scripts/MSDevOps_Scripts/installtools.ps1

---
+++ scripts/MSDevOps_Scripts/installtools.ps1
@@ -0,0 +1,18 @@
+############### Install tentacle ##################
+
+Start-Process msiexec.exe -ArgumentList @('/qn', '/lv C:\FTPFiles\tentacle-log.txt', '/i C:\FTPFiles\Tentacle.msi') -NoNewWindow -Wait
+
+##########Install  Nuget Octoposh #################
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+start-sleep 30
+Install-Module -Name Octoposh -force
+start-sleep 40
+
+###################################################
+
+################# Install IIS #####################
+
+Start-Process msiexec.exe -ArgumentList @('/qn', '/lv C:\FTPFiles\IIS-log.txt', '/i C:\FTPFiles\iisexpress.msi') -NoNewWindow -Wait
+
+###################################################
