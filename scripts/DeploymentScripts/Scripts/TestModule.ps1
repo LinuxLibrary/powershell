091d5d3797d64cb4024a4c9184d253f12643b528
Arjun Shrinivas <vmsnivas@gmail.com>
2018-07-04 12:16:48 +0530

    Modified scripts/DeploymentScripts/Scripts/TestModule.ps1

--- scripts/DeploymentScripts/Scripts/TestModule.ps1
+++ scripts/DeploymentScripts/Scripts/TestModule.ps1
@@ -7,32 +7,32 @@ Try{
 Finally{
 	Write-Host ""
 }
-   $RUser = "lldev\vmsnivas"
-   $RPasswd = "XX-PASS-XX"
-   $RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
-   $RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
-   New-PSDrive -Name Z -PSProvider FileSystem -Root "\\LLDEV3P1WEB01\WIS_FOLDER" -Credential $RCred > $null
+$RUser = "lldev\vmsnivas"
+$RPasswd = "XX-PASS-XX"
+$RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
+$RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\LLDEV3P1WEB01\WIS_FOLDER" -Credential $RCred > $null
 $File = Get-Content "Z:\Automation\Conf\SList.json" -Raw
 $JsonObject = $File | ConvertFrom-Json
 $name = hostname
 foreach ($Server in $JsonObject.Servers){
-    $RComp = $Server.Name
-    Write-Host $RComp
-    $RUser = "lldev\vmsnivas"
-    $RPasswd = "XX-PASS-XX"
-    $RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
-    $RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
-#	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:StopServices} -Credential $RCred
+	$RComp = $Server.Name
+	Write-Host $RComp
+	$RUser = "lldev\vmsnivas"
+	$RPasswd = "XX-PASS-XX"
+	$RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
+	$RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
+	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:StopServices} -Credential $RCred
 	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:CreateFolders} -Credential $RCred
-#	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:StartServices} -Credential $RCred
+	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:StartServices} -Credential $RCred
 }
 
 foreach ($Server in $JsonObject.Servers){
-    $RComp = $Server.Name
-#    Write-Host $RComp
-    $RUser = "lldev\vmsnivas"
-    $RPasswd = "XX-PASS-XX"
-    $RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
-    $RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
-#	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:VerifyServices} -Credential $RCred
+	$RComp = $Server.Name
+	Write-Host $RComp
+	$RUser = "lldev\vmsnivas"
+	$RPasswd = "XX-PASS-XX"
+	$RPass = ConvertTo-SecureString -AsPlainText $RPasswd -Force
+	$RCred = New-Object System.Management.Automation.PSCredential -ArgumentList $RUser,$RPass
+	Invoke-Command -ComputerName $RComp -ScriptBlock ${Function:VerifyServices} -Credential $RCred
 }
